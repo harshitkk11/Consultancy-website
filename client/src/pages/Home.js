5e8bdad6bceabc88ddfae282a9b5a93e42f8a706
harshitkk11 <114001877+harshitkk11@users.noreply.github.com>
2023-08-28 02:45:28 +0530

    content updated

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -6,17 +6,17 @@ import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
 // import { Link } from "@mui/material"
 import img1 from "../assets/images/Business-stratagy.webp"
 import img2 from "../assets/images/digi-trans.webp"
-import img3 from "../assets/images/fin-ad.png"
+import img3 from "../assets/images/experience.png"
 import img4 from "../assets/images/branding.png"
 import img5 from "../assets/images/hr.png"
 import img6 from "../assets/images/dat.webp"
 // import img7 from "../assets/images/contactus.gif"
 import Card from "../components/Card"
-import icon1 from "../assets/images/sales-team.png"
-import icon2 from "../assets/images/transformation.png"
-import icon3 from "../assets/images/public-relations.png"
-import icon4 from "../assets/images/financial-advisor.png"
-import icon5 from "../assets/images/consultation.png"
+import icon1 from "../assets/images/coding.png"
+import icon2 from "../assets/images/content-strategy.png"
+import icon3 from "../assets/images/solutions.png"
+import icon4 from "../assets/images/Custom Development.png"
+import icon5 from "../assets/images/web-development.png"
 import icon6 from "../assets/images/analysis.png"
 
 function Home() {
@@ -69,15 +69,16 @@ function Home() {
                                 backSpeed={50}
                             />
                         </h1>
-                        <h3>Your Partner in Success</h3>
+                        <h4>Empowering Your Online Presence</h4>
                     </div>
 
 
                     <div className="about-home">
-                        <p>At InnovateWeb, we are dedicated to helping businesses and individuals thrive in today's dynamic and ever-evolving landscape.
-                            Our team of experienced consultants brings a wealth of knowledge and expertise to provide tailored solutions that drive growth, 
-                            efficiency, and innovation. With a track record of success across various industries, 
-                            we are your trusted partners in achieving your goals.
+                        <p>At InnovateWeb Advisors, we're not just another web consultancy. 
+                            We are your partners in transforming your digital presence into a 
+                            powerful tool that drives success. Our mission is to provide cutting-edge 
+                            web solutions tailored to your unique needs, ensuring your business stands 
+                            out in the digital landscape.
                         </p>
                     </div>
                     
@@ -88,9 +89,9 @@ function Home() {
                             <StyledButton variant="outlined" color="primary" onClick={handleClick}>
                                 GET IN TOUCH
                             </StyledButton>
-                            <StyledButton variant="outlined" color="primary" onClick={handleClick}>
+                            {/* <StyledButton variant="outlined" color="primary" onClick={handleClick}>
                                 REQUEST A QUOTE
-                            </StyledButton>
+                            </StyledButton> */}
                         </Stack>
                     </ThemeProvider>
                     </div>
@@ -103,61 +104,72 @@ function Home() {
                     <div className="card-container">
                         <Card 
                             image={icon1} 
-                            alt="Business Strategy Consulting"
-                            heading="Business Strategy Consulting"
-                            paragraph="Crafting a winning business strategy is the foundation of success. 
-                            Our consultants work closely with you to analyze market trends, 
-                            competition, and opportunities, helping you define a clear and 
-                            actionable strategy that aligns with your objectives."/>
+                            alt="Web Design and Development"
+                            heading="Web Design and Development"
+                            paragraph=" Our expert team of designers and developers work hand 
+                            in hand to create visually stunning and highly functional websites. 
+                            We take pride in crafting websites that not only capture attention 
+                            but also offer seamless user experiences. From responsive designs to 
+                            e-commerce solutions, we've got you covered."/>
                         <Card 
                             image={icon2} 
-                            alt="Digital Transformation"
-                            heading="Digital Transformation"
-                            paragraph="Embrace the digital age with confidence. 
-                            Our experts guide you through the process of digital transformation, 
-                            from technology integration to optimizing customer experiences, 
-                            ensuring your business remains competitive in the digital landscape.
-                            "/>
+                            alt="Digital Strategy"
+                            heading="Digital Strategy"
+                            paragraph=" The online world is ever-evolving, and we're here to guide 
+                            you through it. Our strategic approach aligns your business goals with
+                             the digital landscape, ensuring that your online presence doesn't just
+                              exist, but thrives. We offer comprehensive digital strategy services, 
+                              including SEO, content strategy, and social media marketing."/>
                         <Card 
                             image={icon3} 
-                            alt="Marketing and Branding"
-                            heading="Marketing and Branding"
-                            paragraph="Stand out in a crowded marketplace. We help you create compelling brand stories, develop effective marketing campaigns, and establish a strong online presence to attract and retain customers."
+                            alt="E-commerce Solutions"
+                            heading="E-commerce Solutions"
+                            paragraph="Ready to take your products online? Our e-commerce expertise 
+                            can help you establish a robust online store that caters to your target 
+                            audience. From setting up secure payment gateways to optimizing product 
+                            listings, we make selling online a breeze."
                         />
                         <Card 
                             image={icon4} 
-                            alt="Financial Advisory"
-                            heading="Financial Advisory"
-                            paragraph="Unlock the full potential of your financial resources. 
-                            Our financial consultants offer insights into managing budgets, 
-                            making smart investments, and improving overall financial health, 
-                             while minimizing risks."
+                            alt="Custom Development"
+                            heading="Custom Development"
+                            paragraph="Your business is unique, and so should be your website. Our 
+                            custom development services give you the freedom to create tailored solutions 
+                            that cater to your specific requirements. Whether it's a customer portal, a 
+                            booking system, or a niche-specific tool, we bring your ideas to life."
                         />
                         <Card 
                             image={icon5} 
-                            alt="Human Resources Consulting"
-                            heading="Human Resources Consulting"
-                            paragraph="Your people are your greatest asset. Our HR consultants assist you in building a motivated workforce, from talent acquisition and development to performance management and employee engagement strategies."
+                            alt="Website Audits and Optimization"
+                            heading="Website Audits and Optimization"
+                            paragraph="Is your existing website not performing as expected? Our team conducts
+                             in-depth website audits to identify bottlenecks and opportunities for improvement.
+                              We then optimize your site for speed, user experience, and search engine visibility,
+                               ensuring it reaches its full potential."
                         />
                         <Card 
                             image={icon6} 
                             alt="Data Analytics and Insights"
                             heading="Data Analytics and Insights"
-                            paragraph="Make informed decisions backed by data. Our data analysts provide in-depth insights that drive business growth, optimize operations, and enhance customer satisfaction."
+                            paragraph="Make informed decisions backed by data. Our data analysts provide 
+                            in-depth insights that drive business growth, optimize operations, and enhance 
+                            customer satisfaction."
                         />
                     </div>
                 </div>
 
 {/* third container */}
-
+                
+                <div className="third-container-heading">Why Choose InnovateWeb Advisors?</div>
                 <div className="third-container">
-                <div className="container-heading">Why Choose InnovateWeb Advisors?</div>
                     <div className="flex-container">
                         <div className="item1">
                             <img src={img1} className="img1" alt="Business Strategy Consulting"/>
                             <div className="item-content">
-                                <h3>Experienced Consultants</h3>
-                                <p>Our team consists of industry veterans with a proven track record of delivering results.</p>
+                                <h3>Experienced Professionals</h3>
+                                <p>Our team comprises seasoned experts in web design, development, digital marketing, 
+                                    and more. With years of industry experience, we know what it takes to succeed in the 
+                                    online realm.</p>
                             </div>
                         </div>
 
@@ -186,8 +198,8 @@ function Home() {
                         <div className="item5">
                             <img src={img5} className="img5" alt="Human Resources Consulting" />
                             <div className="item-content">
-                            <h3>Proven Results</h3>
-                                <p>Our portfolio is filled with success stories of clients who have achieved significant growth and transformation with our guidance.</p>
+                            <h3>Results-Driven</h3>
+                                <p> Your success is our success. We are committed to delivering tangible results that positively impact your business. Whether it's increased website traffic, higher conversion rates, or improved user engagement, we're here to make it happen.</p>
                             </div>
                         </div>
                         <div className="item6">
@@ -212,28 +224,28 @@ function Home() {
 
                 <div className="forth-container">
                     <div className="container-heading">
-                        <p>Get Started on Your Journey to Success</p>
+                        <p>Ready to elevate your online presence?</p>
                     </div>
 
 
                     <div className="content">
-                        <p>
-                            Whether you're a startup looking to establish your presence, 
-                            an established business aiming for growth, 
-                            or an individual seeking personal development, <strong>InnovateWeb Advisors</strong> is here to guide you. 
-                            Contact us today for a consultation and take the first step towards unlocking your full potential.
+                        <p>Contact us today for a free consultation. 
+                            Let's discuss how <strong>InnovateWeb Advisors</strong> 
+                            can take your digital journey to new heights. Together, 
+                            let's build a digital presence that leaves a lasting impression.
                         </p>
                     </div>
 
                     <div className="contact-button">
-                        <StyledButton variant="contained" color="primary" size="large" onClick={handleClick}>
-                                CONTACT US
-                        </StyledButton>
+                        <ThemeProvider theme={theme}>
+                            <StyledButton variant="outlined" color="primary" onClick={handleClick}>
+                                Contact Us
+                            </StyledButton>
+                        </ThemeProvider>
                     </div>
 
                     <div className="content">
-                        <p>Transform challenges into opportunities with <strong>InnovateWeb Advisors</strong>. 
-                            Your success story begins here.</p>
+                        <p>Remember, the online world is waiting and we're here to make sure you shine in it.</p>
                     </div>
                 </div>
             </div>
